43f74233d06d398e06f849b4c4ad90ad990dac8c
dddCrazy87 <n126970@gmail.com>
2025-10-11 00:29:31 +0800

    Init

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GrowthRateQuiz from "./growth_rate_quiz";
+
+function App() {
+  return <GrowthRateQuiz />;
+}
+
+export default App;
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+import GrowthRateQuiz from './growth_rate_quiz';
+export default function App() { return <GrowthRateQuiz />; }
+<h1 className="text-5xl text-blue-600 font-bold">Tailwind Works!</h1>
